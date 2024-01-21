a9ebb19469c484b7bea3a82316ff13d014fa85ce
jjs <jjs815557@gmail.com>
2024-01-22 00:19:39 +0900

    update

--- server.js
+++ server.js
@@ -6,27 +6,30 @@ const port = 3000;
 async function server() {
   const http = require("http").createServer(app);
   const io = new Server(http, { transports: ["websocket"] });
+  // const cors = require("cors");
 
   const roomName = "room1234";
+
+  // app.use(cors());
 
   io.on("connection", (socket) => {
     console.log("socket connected");
     socket.on("join", () => {
+      console.log("joined");
       //클라이언트의 roomName을 받아서 처리해도됨
       socket.join(roomName);
       socket.to(roomName).emit("joined");
     });
     socket.on("offer", (offer) => {
-      socket.to(roomName).emit("offer", offer);
-    });
-
-    socket.on("offer", (offer) => {
+      console.log("offer");
       socket.to(roomName).emit("offer", offer);
     });
     socket.on("answer", (answer) => {
+      console.log("answer");
       socket.to(roomName).emit("answer", answer);
     });
     socket.on("ice", (ice) => {
+      console.log("ice");
       socket.to(roomName).emit("ice", ice);
     });
   });
